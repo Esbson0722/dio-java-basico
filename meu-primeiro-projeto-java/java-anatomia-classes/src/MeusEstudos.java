0e673dfea6c964ee274dc52baaeb70a4ead290f1
binho <esbsonalmeida@gmail.com>
2024-06-06 01:11:43 -0300

    Anatomia das Clases

--- meu-primeiro-projeto-java/java-anatomia-classes/src/MeusEstudos.java
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MeusEstudos.java
@@ -2,6 +2,19 @@ public class MeusEstudos {
 
 public static void main(String [] args) {
     System.out.println("Olá todos estou iniciando meus estudos em Java");
+
+// declaração valida de variáveis
+int numero$um = 1;
+int numero1 = 1;
+int numeroum = 1;
+int longo = 1;
+
+
+
+
+
+
+
 }
     
 }
