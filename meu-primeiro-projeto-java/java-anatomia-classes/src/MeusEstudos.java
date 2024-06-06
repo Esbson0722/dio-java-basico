d3020c76f226bca4c1df021d7c79dcae9baf15dd
binho <esbsonalmeida@gmail.com>
2024-06-06 01:25:03 -0300

    AULA 4

--- meu-primeiro-projeto-java/java-anatomia-classes/src/MeusEstudos.java
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MeusEstudos.java
@@ -1,20 +1,16 @@
 public class MeusEstudos {
 
 public static void main(String [] args) {
-    System.out.println("Olá todos estou iniciando meus estudos em Java");
+    String primeiroNome = "Esbson";
+    String segundoNome = "Almeida";
 
-// declaração valida de variáveis
-int numero$um = 1;
-int numero1 = 1;
-int numeroum = 1;
-int longo = 1;
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
 
-
-
-
-
-
-
+    System.out.println(nomeCompleto);
 }
-    
+
+public static String nomeCompleto (String primeiroNome, String seundoNome) {
+    return "Resutado do método" + primeiroNome.concat(" "). concat(seundoNome);
+}
+
 }
